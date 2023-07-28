390bb00029dbe3506eb58f6ee1d4fc133729a6a2
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-07-28 16:15:55 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import './index.css';
 
-import {
-  addTaskToList,
-  saveTasksToStorage,
-} from './modules/addTaskToList.js'; // Updated import statement
-// import addTaskToList from './modules/addTaskToList.js';
-// import { saveTasksToStorage, loadTasksFromStorage } from './modules/localstorage.js';
-import { loadTasksFromStorage } from './modules/localstorage.js';
+// import {
+//   addTaskToList,
+//   saveTasksToStorage,
+// } from './modules/addTaskToList.js'; // Updated import statement
+import { addTaskToList } from './modules/addTaskToList.js';
+import { saveTasksToStorage, loadTasksFromStorage } from './modules/localstorage.js';
+// import { loadTasksFromStorage } from './modules/localstorage.js';
 import clearCompletedTasks from './modules/remove.js';
 
 let tasks = loadTasksFromStorage();
 
 const taskList = document.getElementById('todo-task-list');
 const taskInput = document.querySelector('.add-task input');
+// const taskInput = document.getElementById('new-task-add');
 
 taskInput.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
@@ -35,12 +36,12 @@ taskInput.addEventListener('keyup', (event) => {
   }
 });
 
-const btnRefresh = document.getElementById('btn-refresh');
+const btnRefresh = document.getElementById('refresh');
 btnRefresh.addEventListener('click', () => {
   window.location.reload();
 });
 
-const btnClear = document.querySelector('.clear-items button');
+const btnClear = document.querySelector('.wipe-all-out');
 btnClear.addEventListener('click', () => {
   clearCompletedTasks(tasks);
 });
